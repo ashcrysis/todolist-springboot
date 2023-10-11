e4a54fa02bcaad2aa65c2c4c295d77fef8373a46
asher gabriela <109311931+asheryyy@users.noreply.github.com>
2023-10-11 19:58:19 -0300

    v-09 : Now the application has an task handler

---
+++ src/main/java/br/com/ashermartins/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,31 @@
+package br.com.ashermartins.todolist.filter;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        // Receive Login (username and password)
+        var authorization = request.getHeader("Authorization");
+        authorization.substring("Basic".length()).trim();
+        // Validate Username
+
+        // Validate password
+
+        filterChain.doFilter(request, response);
+
+    }
+
+}
