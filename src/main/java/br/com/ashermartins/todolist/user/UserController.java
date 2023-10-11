4a6f56a955bf14d38800d21088d9c672ba5415f1
asher gabriela <109311931+asheryyy@users.noreply.github.com>
2023-10-10 23:57:32 -0300

    v-07 : ResponseEntity to handle HTTStatus implemented

--- src/main/java/br/com/ashermartins/todolist/user/UserController.java
+++ src/main/java/br/com/ashermartins/todolist/user/UserController.java
@@ -1,10 +1,14 @@
 package br.com.ashermartins.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.core.ipc.http.HttpSender.Response;
 
 @RestController
 @RequestMapping("/users")
@@ -14,17 +18,19 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if (user != null) {
 
             System.out.println("User alrealdy exists in database");
-            return null;
+            // Error message
+            // Status CODE
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User alrealdy exists.");
 
         }
 
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
