787c0ecdab10eb5857c51af2d0b527736d16262a
asher gabriela <109311931+asheryyy@users.noreply.github.com>
2023-10-10 23:29:49 -0300

    v-04 : Generated Value Added and ID added

--- src/main/java/br/com/ashermartins/todolist/user/UserModel.java
+++ src/main/java/br/com/ashermartins/todolist/user/UserModel.java
@@ -1,10 +1,21 @@
 package br.com.ashermartins.todolist.user;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
+    @Column(name = "usuario")
     public String username;
     public String name;
     public String password;
