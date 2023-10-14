ad33fe145653e0aa4b2575b13acc632ab8fd3bf8
asher gabriela <109311931+asheryyy@users.noreply.github.com>
2023-10-14 14:09:22 -0300

    v-17 : Exception Handler update

--- src/main/java/br/com/ashermartins/todolist/errors/ExceptionHandlerController.java
+++ src/main/java/br/com/ashermartins/todolist/errors/ExceptionHandlerController.java
@@ -12,7 +12,7 @@ public class ExceptionHandlerController {
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMostSpecificCause().getMessage());
 
     }
 }
